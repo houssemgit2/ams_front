90e116777b318be02addf2f7ece9a83798143da5
houssemgit2 <fethiaboulfarah@gmail.com>
2026-04-16 10:50:00 +0200

    fix: navbar home routing

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Provider } from '../../../models';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
