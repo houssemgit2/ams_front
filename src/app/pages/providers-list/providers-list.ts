255d240c948627a42579f0ac9f05b82b60204f69
houssemgit2 <fethiaboulfarah@gmail.com>
2026-04-16 12:43:59 +0200

    feat: plug delete  provider API

--- src/app/pages/providers-list/providers-list.ts
+++ src/app/pages/providers-list/providers-list.ts
@@ -3,28 +3,50 @@ import { BehaviorSubject } from 'rxjs';
 import { ProviderService } from '../../services/providerService';
 import { CommonModule } from '@angular/common';
 import { Provider } from '../../../models';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-providers-list',
   imports: [CommonModule],
   templateUrl: './providers-list.html',
-  styleUrl: './providers-list.scss',
+  styleUrls: ['./providers-list.scss'],
 })
 export class ProvidersList implements OnInit {
-  listProviders = new BehaviorSubject<Provider[] | null>(null);
+  listProviders = new BehaviorSubject<Provider[]>([]);
 
-  constructor(private providerService: ProviderService) {}
+  constructor(
+    private providerService: ProviderService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
+    this.loadProviders();
+  }
+
+  loadProviders() {
     this.providerService.getProviders().subscribe({
-      next: (data: any) => {
+      next: (data: Provider[]) => {
         this.listProviders.next(data);
-        console.log(this.listProviders);
       },
       error: (err) => {
         console.log('Erreur de récupération des providers', err);
       },
     });
+  }
+
+  onDelete(id: number) {
+    if (confirm('Are you sure you want to delete this provider?')) {
+      this.providerService.deleteProvider(id).subscribe({
+        next: () => {
+          this.loadProviders();
+        },
+        error: (err) => console.log(err),
+      });
+    }
+  }
+
+  onEdit(id: number) {
+    this.router.navigate(['/home/providers/edit', id]);
   }
 }
--- src/app/services/providerService.ts
+++ src/app/services/providerService.ts
@@ -1,17 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Provider } from '../../models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProviderService {
-  constructor(private htpp: HttpClient) {}
+  private apiUrl = 'http://localhost:8080/providers';
 
-  getProviders() {
-    return this.htpp.get('http://localhost:8080/providers/');
+  constructor(private http: HttpClient) {}
+
+  getProviders(): Observable<Provider[]> {
+    return this.http.get<Provider[]>(`${this.apiUrl}/`);
   }
 
-  saveProvider(provider: any) {
-    return this.htpp.post('http://localhost:8080/providers/', provider);
+  saveProvider(provider: Provider): Observable<Provider> {
+    return this.http.post<Provider>(`${this.apiUrl}/`, provider);
+  }
+
+  deleteProvider(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  getProviderById(id: number): Observable<Provider> {
+    return this.http.get<Provider>(`${this.apiUrl}/${id}`);
+  }
+
+  updateProvider(id: number, provider: Provider): Observable<Provider> {
+    return this.http.put<Provider>(`${this.apiUrl}/${id}`, provider);
   }
 }
