cd26dcbe0b80a4b32a2b5045c57b679587de31ee
houssemgit2 <fethiaboulfarah@gmail.com>
2026-04-17 18:42:03 +0200

    fix: build bugs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './pages/home/home';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
-import { routes } from './app/app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())],
-};
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
